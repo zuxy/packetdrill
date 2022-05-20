54703a933dc88c0c9ced034d11157d41272f97c5
Shuo Chen <shuochen@google.com>
2022-05-20 22:27:41 +0000

    packetdrill: Switch to Python3.
    Original-by: John Sperbeck
    Signed-off-by: Shuo Chen <shuochen@google.com>

--- gtests/net/tcp/common/set_sysctls.py
+++ gtests/net/tcp/common/set_sysctls.py
@@ -1,21 +1,26 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
-import commands
 import os
+import subprocess
 import sys
 
-pppid = int(os.popen("ps -p %d -oppid=" % os.getppid()).read().strip())
-filename = '/tmp/sysctl_restore_%d.sh' % pppid
+# Sometimes shell skips fork() as an optimization.
+# https://unix.stackexchange.com/questions/466496/
+if os.environ.get('SHELL_SKIPS_FORK'):
+  filename = '/tmp/sysctl_restore_%d.sh' % os.getppid()
+else:
+  pppid = int(os.popen("ps -p %d -oppid=" % os.getppid()).read().strip())
+  filename = '/tmp/sysctl_restore_%d.sh' % pppid
 
 restore_file = open(filename, 'w')
-print >> restore_file, '#!/bin/bash'
+print('#!/bin/bash', file=restore_file)
 
 for a in sys.argv[1:]:
   sysctl = a.split('=')
 
   # save current value
-  cur_val = commands.getoutput('cat ' + sysctl[0])
-  print >> restore_file, 'echo "%s" > %s' % (cur_val, sysctl[0])
+  cur_val = subprocess.check_output(['cat', sysctl[0]], universal_newlines=True)
+  print('echo "%s" > %s' % (cur_val.strip(), sysctl[0]), file=restore_file)
 
   # set new value
   cmd = 'echo "%s" > %s' % (sysctl[1], sysctl[0])
